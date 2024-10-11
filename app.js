50c81975a4fe816d8f98736cd9d41ddfacd396e9
Mind Strata <mindstrata@gmail.com>
2024-10-11 16:29:26 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", async (req, res, next) => {
+  res.send("Hello World...");
+});
+
+app.get("/getdata", async (req, res, next) => {
+  res.status(200).send({
+    success: true,
+    message: "Data fetched successfully",
+    data: [
+      {
+        id: 1,
+        title: "Express.js tutorial 2024",
+        comments: 12,
+      },
+      {
+        id: 2,
+        title: "Node.js tutorial 2024",
+        comments: 3,
+      },
+    ],
+    error: null,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ gitDocs/basic.md
@@ -0,0 +1,17 @@
+### Basic setup
+
+```bash
+# Git configuration
+
+# To set your name
+git config --global user.name "Your Name"
+
+# To set email id
+git config --global user.email "example@domain.com"
+
+# To see the configuration list
+git config --list
+
+# To clone a repo
+git clone <LINK>
+```
