79d2e441eac889c742943bc901490cf152e55d33
Mind Strata <mindstrata@gmail.com>
2024-10-15 12:09:06 +0530

    connect mongodb database to codebase

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-import express from "express";
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
+import express from "express";
+import "./db/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
