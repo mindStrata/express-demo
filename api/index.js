ab9292a3735d7a320eb912672a91f89f0639033a
Mind Strata <mindstrata@gmail.com>
2024-10-18 13:07:45 +0530

    update codebase

--- api/index.js
+++
@@ -1,8 +0,0 @@
-const app = require("express")();
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
-module.exports = app;
